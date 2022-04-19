65e0f283801b93c19929923d68d71d2321262934
Blake Douglass <blakedouglass@Blakes-Mac-mini.local>
2022-04-19 10:02:01 -0700

    added local css

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -15,6 +15,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 500px;
+  height: 400px;
+  overflow-y: scroll;
+  border: 3px solid black;
 }
 
 nav {
